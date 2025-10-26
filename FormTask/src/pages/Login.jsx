3ea985e9040ba14d67cdc4cbb11a0a004a11ed6b
Dada Benjamin <166409309+Dadben08@users.noreply.github.com>
2025-10-26 14:59:32 +0100

    Update Login.jsx

--- FormTask/src/pages/Login.jsx
+++ FormTask/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     setLoading(true);
     setError("");
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+      const res = await axios.post("https://taskform-backend.onrender.com/api/auth/login", { email, password });
       localStorage.setItem("token", res.data.token);
       window.location.href = "/dashboard";
     } catch (err) {
